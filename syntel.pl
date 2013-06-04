83ab89bc052579b55b3aba124a1b0568802f972e
Dustin L. Howett <dustin@howett.net>
2013-06-03 19:08:45 -0700

    Slightly more complex code: test printf prototype and call!

--- syntel.pl
+++ syntel.pl
@@ -17,13 +17,20 @@ $root->push($y->declaration);
 
 my $f = Function->new("whatever", "int");
 $f->body->push(Return->new(32));
-$root->push($f);
+
+$root->push($f->prototype);
+
+my $printf = Function->new("printf", "int", [Variable->new("fmt", "char *"), Vararg->new()]);
+$root->push($printf->prototype);
 
 my $main = Function->new("main", "int", [Variable->new("argc", "int"), Variable->new("argv", "char**")]);
 $main->body->push($x->assign(10));
 $main->body->push($y->assign($f->call()));
+$main->body->push($printf->call([String->new("x == %d, y == %d\n"), $x, $y]));
 $main->body->push($x->assign($y));
 $main->body->push(Return->new($y));
 $root->push($main);
+
+$root->push($f);
 
 print $root->emit();
