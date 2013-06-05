f25cacb83c3ceee5b2488cb30c4ed458f4a3b054
Dustin L. Howett <dustin@howett.net>
2013-06-05 01:22:37 -0700

    Cooler test script.

--- test.sh
+++ test.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env zsh
 setopt multios
+zmodload zsh/stat
+
 t=$(mktemp /tmp/syntelXXXXXXX)
+synout=$(mktemp /tmp/syntelXXXXXXX)
+errout=$(mktemp /tmp/syntelXXXXXXX)
+
+exec {synfd}>&$synout
+exec {errfd}>&$errout
+
+./syntel.pl 1>&$synfd | gcc -o $t -x c - {errfd}>&2
+
+exec {synfd}>&-
+exec {errfd}>&-
+
 echo "############ GENERATED"
-{ ./syntel.pl 1>&2 | gcc -o $t -x c - } 2>&1
+cat $synout
+if [[ $(zstat +size $errout) -gt 0 ]]; then
+	echo "############ WARNINGS"
+	cat $errout
+fi
 echo "############ OUTPUT"
 chmod +x $t
 $t
-rm $t
+
+rm $t $synout $errout
