c02d2e059dad8f724476be7ce3bfc191875f1481
Dustin L. Howett <dustin@howett.net>
2013-06-05 01:33:41 -0700

    Add assignment to Declare

--- Declare.pm
+++ Declare.pm
@@ -8,12 +8,17 @@ sub new {
 	my $pkg = ref $proto || $proto;
 	my $self = {};
 	$self->{VARIABLE} = shift;
+	$self->{EXPR} = shift;
 	return bless $self, $pkg
 }
 
 sub emit {
 	my $self = shift;
-	return $self->{VARIABLE}->{TYPE}." ".$self->{VARIABLE}->{NAME};
+	my $r = $self->{VARIABLE}->{TYPE}." ".$self->{VARIABLE}->{NAME};
+	if(defined $self->{EXPR}) {
+		$r .= " = ".Util::expr($self->{EXPR});
+	}
+	return $r;
 }
 
 1;
