a45974c9fd25e265a999b8d4bdb2d7b5b88eb848
Dustin L. Howett <dustin@howett.net>
2013-06-10 16:53:40 -0700

    An Expression DOES TypedExpression if it can("type")

--- Expression.pm
+++ Expression.pm
@@ -3,6 +3,12 @@ use strict;
 use warnings;
 use parent qw(Statement);
 
+sub DOES {
+	my ($s, $does) = @_;
+	return 1 if $does eq "TypedExpression" && (ref $s) && defined $s->can("type");
+	return $s->SUPER::DOES($does);
+}
+
 sub expr {
 	my $self = shift;
 	return "<EXPR>";
