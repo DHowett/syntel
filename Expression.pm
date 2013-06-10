cc283bfac1f5cbc4231bac137c7ab62e679af601
Dustin L. Howett <dustin@howett.net>
2013-06-10 16:58:57 -0700

    Add TypedExpression for any other expression that does not natively bear a type.

--- Expression.pm
+++ Expression.pm
@@ -20,3 +20,31 @@ sub emit {
 }
 
 1;
+
+package TypedExpression;
+use strict;
+use warnings;
+use parent -norequire, "Expression";
+
+sub new {
+	my $proto = shift;
+	my $pkg = ref $proto || $proto;
+	my $self = {};
+	$self->{EXPRESSION} = shift;
+	$self->{TYPE} = shift;
+	return bless $self, $pkg
+}
+
+sub expression {
+	return $_[0]->{EXPRESSION};
+}
+
+sub type {
+	return $_[0]->{TYPE};
+}
+
+sub expr {
+	return $_[0]->expression->expr;
+}
+
+1;
