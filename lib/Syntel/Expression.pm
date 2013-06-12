6c7be9b3c228c0b0b76fc01eb54810c7e59fd619
Dustin L. Howett <dustin@howett.net>
2013-06-12 03:25:02 -0700

    Expressions are dereferenceable if they bear pointer types.

--- lib/Syntel/Expression.pm
+++ lib/Syntel/Expression.pm
@@ -4,6 +4,21 @@ use warnings;
 use parent qw(Syntel::Statement);
 use role qw(Expression);
 
+use Syntel::PrefixOperator;
+
+sub conformsToRole {
+	my $self = shift;
+	my $role = shift;
+	return 0 if !ref $self;
+	return 1 if $role eq "Dereferenceable" && $self->type->DOES("Pointer");
+	return 0;
+}
+
+sub dereference {
+	my $self = shift;
+	return Syntel::PrefixOperator->new('*', $self)->typed($self->type->innerType);
+}
+
 sub typed {
 	my $self = shift;
 	$self->{TYPE} = shift;
